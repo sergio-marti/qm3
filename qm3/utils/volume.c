fd5243cb550125037937b8bf872fd7a4bab1663d
BuildTools <unconfigured@null.spigotmc.org>
2018-11-16 10:29:10 +0100

    added cube generation for molecular volumes (_volume.so)

--- qm3/utils/volume.c
+++ qm3/utils/volume.c
@@ -42,10 +42,12 @@ void* __fill_atoms( void *args ) {
 }
 
 
-static PyObject* __molecular( PyObject *self, PyObject *args ){
-	PyObject	*o_xyz, *o_rad;
+static PyObject* __molecular_grid( PyObject *self, PyObject *args ){
+	PyObject	*o_xyz, *o_rad, *o_flg;
 	long		i, j, k, l, l3, cpu, siz, rad, rd2, *rng, nit, knd = 0;
-	double		mad, tmp, cnt, vol = 0.0, err = 0.0, cte = 4.1887902047863905;
+	double		mad, tmp, cnt;
+	double		cte = 4.1887902047863905;
+	double		vol = 0.0, err = 0.0, exc = 0.0;
 	double		bmax[3] = { -9999., -9999., -9999. };
 	double		bmin[3] = { +9999., +9999., +9999. };;
 	long		npt[3];
@@ -58,14 +60,16 @@ static PyObject* __molecular( PyObject *self, PyObject *args ){
 	targ		*arg;
 	time_t		t0;
 
-	if( PyArg_ParseTuple( args, "lOOd", &cpu, &o_rad, &o_xyz, &dsp ) ) {
+	o_flg = Py_False;
+	if( PyArg_ParseTuple( args, "lOOd|O", &cpu, &o_rad, &o_xyz, &dsp, &o_flg ) ) {
 
 		t0 = time( NULL );
-fprintf(stderr,"CPU: %ld\n",cpu);
-fprintf(stderr,"DSP: %lf\n",dsp);
 		d3  = dsp * dsp * dsp;
 		siz = (long) PyList_Size( o_rad );
 		crd = (double*) malloc( 3 * siz * sizeof( double ) );
+		if( siz < cpu ) { cpu = 1; }
+fprintf(stderr,"CPU: %ld\n",cpu);
+fprintf(stderr,"DSP: %lf\n",dsp);
 
 		hsh = (sph_lst**) malloc( siz * sizeof( sph_lst* ) );
 		sph = (sph_lst*)  malloc( sizeof( sph_lst ) );
@@ -126,7 +130,7 @@ fprintf(stderr,"DSP: %lf\n",dsp);
 				bmax[i] = max( bmax[i], crd[l3+i] );
 			}
 		}
-		mad *= 0.75;
+		mad *= 1.1;
 		for( i = 0; i < 3; i++ ) { 
 			bmin[i] -= mad; bmax[i] += mad;
 			npt[i] = (long)( ( bmax[i] - bmin[i] ) / dsp ) + 1;
@@ -161,7 +165,36 @@ fprintf(stderr,"ERR: %.3lf _A^3\n",err*siz);
 		}
 		for( l = 0; l < cpu; l++ ) pthread_join( pid[l], NULL );
 
-		for( l = 0; l < npt[0] * npt[1] * npt[2]; l++ ) if( grd[l] > 0 ) { vol += 1.0; }
+	if( o_flg == Py_True ) {
+		FILE* fd = fopen( "volume.cube", "wt" );
+		fprintf( fd, "QM3:\n-- Molecular Volume --\n" );
+		tmp = 0.52917721092;
+		fprintf( fd, "%5ld%12.6lf%12.6lf%12.6lf\n", siz, bmin[0] / tmp, bmin[1] / tmp, bmin[2] / tmp );
+		fprintf( fd, "%5ld%12.6lf%12.6lf%12.6lf\n", npt[0], dsp / tmp, 0.0, 0.0 );
+		fprintf( fd, "%5ld%12.6lf%12.6lf%12.6lf\n", npt[1], 0.0, dsp / tmp, 0.0 );
+		fprintf( fd, "%5ld%12.6lf%12.6lf%12.6lf\n", npt[2], 0.0, 0.0, dsp / tmp );
+		for( l = 0; l < siz; l++ )
+			fprintf( fd, "%5ld%12.6lf%12.6lf%12.6lf%12.6lf\n", (long)0, 0.0,
+				crd[3*l] / tmp, crd[3*l+1] / tmp, crd[3*l+2] / tmp );
+		nit = npt[0] * npt[1];
+		l = 0;
+		for( i = 0; i < npt[0]; i++ ) {
+			for( j = 0; j < npt[1]; j++ ) {
+				for( k = 0; k < npt[2]; k++ ) {
+					fprintf( fd, "%13.5le", (float) grd[i + j * npt[0] + k * nit] );
+					l++;
+					if( l%6 == 0 ) { fprintf( fd, "\n" ); }
+				}
+			}
+		}
+		fclose( fd );
+		}
+
+		for( l = 0; l < npt[0] * npt[1] * npt[2]; l++ ) {
+			if( grd[l] > 0 ) { vol += 1.0; }
+//			if( grd[l] > 1 ) { exc += 1.0; }
+		}
+//fprintf(stderr,"EXC: %lf _A^3\n",exc*d3);
 fprintf(stderr,"VOL: %lf _A^3\n",vol*d3);
 
 		free( pid ); free( rng ); free( arg );
@@ -188,8 +221,75 @@ fprintf( stderr, "TIM: %ld _sec\n", time( NULL ) - t0 );
 }
 
 
+/*
+This version is not useful for "real" problems in which intersections take
+	place among more than 2 spheres...
+The result is that the multiple exclussion volumes are considered more than one
+	time, thus providing smaller volumes (compared to the grid method...)
+
+Follow (someday) these analytical formulas:
+
+	MOLECULARPHYSICS,1991, VOL. 72, No. 6, 1313-1345
+>>	J. Phys. Chem. 1987, 91, 4121-4122
+	Mathematical Notes, vol. 69, no. 3, 2001, pp. 421–428
+	Nano Studies, 2015, 11, 111-126
+*/
+static PyObject* __molecular_spheres( PyObject *self, PyObject *args ){
+	PyObject	*o_xyz, *o_rad;
+	long		i, j, i3, j3, siz;
+	double		cte = 1.0471975511965976;
+	double		vol = 0.0, exc = 0.0;
+	double		*crd, *r, *r2, d, d2, hi, hj;
+	time_t		t0;
+
+	if( PyArg_ParseTuple( args, "OO", &o_rad, &o_xyz ) ) {
+
+		t0 = time( NULL );
+		siz = (long) PyList_Size( o_rad );
+		crd = (double*) malloc( 3 * siz * sizeof( double ) );
+		r   = (double*) malloc( siz * sizeof( double ) );
+		r2  = (double*) malloc( siz * sizeof( double ) );
+
+		for( i = 0; i < siz; i++ ) {
+			r[i]  = PyFloat_AsDouble( PyList_GetItem( o_rad, i ) );
+			r2[i] = r[i] * r[i];
+			vol  += r2[i] * r[i];
+			i3    = i * 3;
+			for( j = 0; j < 3; j++ ) {
+				crd[i3+j] = PyFloat_AsDouble( PyList_GetItem( o_xyz, i3+j ) );
+			}
+		}
+		for( i = 0; i < siz - 1; i++ ) {
+			i3 = i * 3;
+			for( j = i + 1; j < siz; j++ ) {
+				j3  = j * 3;
+				d2  = ( crd[i3]   - crd[j3]   ) * ( crd[i3]   - crd[j3]   );
+				d2 += ( crd[i3+1] - crd[j3+1] ) * ( crd[i3+1] - crd[j3+1] );
+				d2 += ( crd[i3+2] - crd[j3+2] ) * ( crd[i3+2] - crd[j3+2] );
+				if( d2 < r2[i] + r2[j] + 2.0 * r[i] * r[j] ) {
+					d    = sqrt( d2 );
+					hi   = r[i] * ( 1.0 - ( r2[i] + d2 - r2[j] ) / ( 2.0 * r[i] * d ) );
+					hj   = r[j] * ( 1.0 - ( r2[j] + d2 - r2[i] ) / ( 2.0 * r[j] * d ) );
+					exc += hi * hi * ( 3.0 * r[i] - hi ) + hj * hj * ( 3.0 * r[j] - hj );
+				}
+			}
+		}
+		vol *= 4.0 * cte;
+		exc *= cte;
+fprintf(stderr,"SPH: %lf _A^3\n",vol);
+fprintf(stderr,"EXC: %lf _A^3\n",exc);
+		vol -= exc;
+fprintf(stderr,"VOL: %lf _A^3\n",vol);
+		free( crd ); free( r ); free( r2 );
+fprintf( stderr, "TIM: %ld _sec\n", time( NULL ) - t0 );
+		return( Py_BuildValue( "d", vol ) );
+	}
+	Py_INCREF( Py_None ); return( Py_None );
+}
+
+
 static struct PyMethodDef methods [] = {
-    { "molecular", (PyCFunction)__molecular, METH_VARARGS },
+    { "molecular",      (PyCFunction)__molecular_grid, METH_VARARGS },
     { 0, 0, 0 }
 };
 
