b6dfced9247020e1836ba394d285178fb600e156
BuildTools <unconfigured@null.spigotmc.org>
2019-04-09 16:11:41 +0200

    msi: desconection oriented

--- qm3/utils/msi.py
+++ qm3/utils/msi.py
@@ -8,146 +8,198 @@ if( sys.version_info[0] == 2 ):
 	range = xrange
 import	socket
 import	struct
+import	time
+import	threading
 
 
 class server:
-	def __init__( self, ncpu, pnum = 6969, srvr = None ):
-		self.ncpu = ncpu
-		self.node = 0
-		self.slen = 1024
-		if( srvr ):
-			hstn = srvr
-		else:
-			hstn = socket.gethostbyname( socket.gethostname() )
-		sys.stderr.write( "[server] listening at: %s\n"%( hstn ) )
-		sys.stderr.flush()
-		sckt = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
-		sckt.bind( ( hstn, pnum ) )
-		self.chld = {}
-		while( len( self.chld ) < ncpu - 1 ):
-			sckt.listen( ncpu )
-			chld, addr = sckt.accept()
-			node = len( self.chld ) + 1
-			self.chld[node] = chld
-			self.__send( chld, struct.pack( "i", node ) )
-			sys.stderr.write( "[server] added: %d/%d\n"%( node, ncpu - 1 ) )
-			sys.stderr.flush()
-		sckt.close()
-		sys.stderr.write( "[server] done!\n" )
-		sys.stderr.flush()
+	def __recv( self, sckt ):
+		msg = sckt.recv( self.slen )
+		try:
+			cmd = msg[0:1]
+			who = int( msg[1:4] )
+			dst = int( msg[4:7] )
+			siz = int( msg[7:17] )
+			dat = msg[17:]
+			while( len( dat ) < siz ):
+				dat += sckt.recv( self.slen )
+			return( cmd, who, dst, dat[0:siz] )
+		except:
+			return( b"#", -1, -1, b"" )
 
 
-	def stop( self ):
-		for i in sorted( self.chld ):
-			self.chld[i].close()
-
-
-	def __send( self, sck, msg ):
+	def __send( self, sckt, msg ):
 		s = len( msg )
 		j = 0
 		for i in range( s // self.slen ):
-			sck.send( msg[j:j+self.slen] )
+			sckt.sendall( msg[j:j+self.slen] )
 			j += self.slen
 		s %= self.slen
 		if( s != 0 ):
-			sck.send( msg[j:] + b"0" * ( self.slen - s ) )
+			sckt.sendall( msg[j:] + b"0" * ( self.slen - s ) )
 
 
-	def __recv( self, sck, siz ):
-		msg = sck.recv( self.slen )
-		while( len( msg ) < siz ):
-			msg += sck.recv( self.slen )
-		return( msg[0:siz] )
+	def __serve( self, chld ):
+		cmd, who, dst, dat = self.__recv( chld )
+		if( cmd == b"#" or who < 0 or who >= self.ncpu ):
+			chld.close()
+			sys.stderr.write( "[server] rejected invalid node(%d) or malformed message...\n"%( who ) )
+			sys.stderr.flush()
+			return()
+		self.lock.acquire()
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		if( cmd == b"B" ):
+			self.barc[who] += 1
+			if( not( self.barc[who] in self.barb and self.barc[who] in self.bard ) ):
+				self.barb[self.barc[who]] = [ 0 for i in range( self.ncpu ) ]
+				self.bard[self.barc[who]] = True
+				sys.stderr.write( "[server] barrier %d +++ (node: %03d)\n"%( self.barc[who], who ) )
+				sys.stderr.flush()
+			self.barb[self.barc[who]][who] = 1
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		elif( cmd == b"P" ):
+			if( self.bard[self.barc[who]] ):
+				self.bard[self.barc[who]] = sum( self.barb[self.barc[who]] ) < self.ncpu
+				if( self.bard[self.barc[who]] ):
+					self.__send( chld, b"10" )
+				else:
+					self.barb[self.barc[who]][who] = 0
+					self.__send( chld, b"00" )
+			else:
+				self.barb[self.barc[who]][who] = 0
+				if( sum( self.barb[self.barc[who]] ) == 0 ):
+					sys.stderr.write( "[server] barrier %d --- (node: %03d)\n"%( self.barc[who], who ) )
+					sys.stderr.flush()
+					del self.barb[self.barc[who]]
+					del self.bard[self.barc[who]]
+				self.__send( chld, b"00" )
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		elif( cmd == b"W" ):
+			if( self.data[dst][who] == b"" ):
+				self.data[dst][who] = dat
+				self.__send( chld, b"10" )
+			else:
+				self.__send( chld, b"00" )
+		elif( cmd == b"R" ):
+			if( self.data[who][dst] != b"" ):
+				self.__send( chld, b"1" + self.data[who][dst] )
+				self.data[who][dst] = b""
+			else:
+				self.__send( chld, b"00" )
+		self.lock.release()
+		chld.close()
 
 
-	def barrier( self ):
-		out = []
-		for i in sorted( self.chld ):
-			out.append( struct.unpack( "i", self.__recv( self.chld[i], 4 ) )[0] )
-		for i in sorted( self.chld ):
-			self.__send( self.chld[i], struct.pack( "i", 1 ) )
-		return( sum( out ) == len( self.chld ) )
-
-
-	def send_r8( self, who, lst ):
-		msg = b""
-		for r in lst:
-			msg += struct.pack( "d", r )
-		self.__send( self.chld[who], msg )
-
-
-	def recv_r8( self, who, siz ):
-		return( list( struct.unpack( "%dd"%( siz ), self.__recv( self.chld[who], siz * 8 ) ) ) )
-
-
-	def send_i4( self, who, lst ):
-		msg = b""
-		for r in lst:
-			msg += struct.pack( "i", r )
-		self.__send( self.chld[who], msg )
-
-
-	def recv_i4( self, who, siz ):
-		return( list( struct.unpack( "%di"%( siz ), self.__recv( self.chld[who], siz * 4 ) ) ) )
-
-
+	def __init__( self, ncpu, inet = ( "", 6969 ), unix = None ):
+		self.ncpu = ncpu
+		self.slen = 1024
+		self.data = [ [ b"" for j in range( self.ncpu ) ] for i in range( self.ncpu ) ]
+		self.barb = {}
+		self.bard = {}
+		self.barc = [ -1 for i in range( self.ncpu ) ]
+		self.lock = threading.Lock()
+		if( unix ):
+			sys.stderr.write( "[server] listening at: %s\n"%( unix ) )
+			sys.stderr.flush()
+			self.sckt = socket.socket( socket.AF_UNIX, socket.SOCK_STREAM )
+			self.sckt.bind( unix )
+		else:
+			if( inet[0] == "" ):
+				inet[0] = socket.gethostbyname( socket.gethostname() )
+			sys.stderr.write( "[server] listening at: %s\n"%( str( inet ) ) )
+			sys.stderr.flush()
+			self.sckt = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+			self.sckt.bind( inet )
+		while( True ):
+			self.sckt.listen( ncpu * 10 )
+			chld, addr = self.sckt.accept()
+			threading.Thread( target = self.__serve, args = ( chld, ) ).start()
+#		self.sckt.close()
+#		sys.stderr.write( "[server] done!\n" )
+#		sys.stderr.flush()
 
 
 
 class client:
-	def __init__( self, hstn, pnum = 6969 ):
-		self.slen = 1024
-		self.sckt = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
-		self.sckt.connect( ( hstn, pnum ) )
-		self.node = struct.unpack( "i", self.__recv( 4 ) )[0]
-		sys.stderr.write( "[client:%d] connected!\n"%( self.node ) )
-		sys.stderr.flush()
-
-
-	def stop( self ):
-		self.sckt.close()
-
-
-	def __send( self, msg ):
+	def __send( self, sckt, msg ):
 		s = len( msg )
 		j = 0
 		for i in range( s // self.slen ):
-			self.sckt.send( msg[j:j+self.slen] )
+			sckt.sendall( msg[j:j+self.slen] )
 			j += self.slen
 		s %= self.slen
 		if( s != 0 ):
-			self.sckt.send( msg[j:] + b"0" * ( self.slen - s ) )
+			sckt.sendall( msg[j:] + b"0" * ( self.slen - s ) )
 
 
-	def __recv( self, siz ):
-		msg = self.sckt.recv( self.slen )
-		while( len( msg ) < siz ):
-			msg += self.sckt.recv( self.slen )
-		return( msg[0:siz] )
+	def __recv( self, sckt, siz ):
+		msg = sckt.recv( self.slen )
+		if( msg[0:1] == b"1" ):
+			while( len( msg ) <= siz ):
+				msg += sckt.recv( self.slen )
+			return( True, msg[1:siz+1] )
+		else:
+			return( False, b"" )
+
+
+	def __init__( self, node, inet = ( "", 6969 ), unix = None ):
+		self.node = node
+		self.slen = 1024
+		if( unix ):
+			self.kind = socket.AF_UNIX
+			self.addr = unix
+		else:
+			if( inet[0] == "" ):
+				inet[0] = socket.gethostbyname( socket.gethostname() )
+			self.kind = socket.AF_INET
+			self.addr = inet
 
 
 	def barrier( self ):
-		self.__send( struct.pack( "i", 1 ) )
-		return( struct.unpack( "i", self.__recv( 4 ) )[0] == 1 )
+		sckt = socket.socket( self.kind, socket.SOCK_STREAM )
+		sckt.connect( self.addr )
+		self.__send( sckt, b"B%03d00000"%( self.node ) )
+		sckt.close()
+		flg = True
+		while( flg ):
+			time.sleep( 0.1 )
+			sckt = socket.socket( self.kind, socket.SOCK_STREAM )
+			sckt.connect( self.addr )
+			self.__send( sckt, b"P%03d00000"%( self.node ) )
+			flg, dat = self.__recv( sckt, 1 )
+			sckt.close()
 
 
-	def send_r8( self, lst ):
-		msg = b""
+	def send( self, dst, lst ):
+		msg = b"W%03d%03d%010d"%( self.node, dst, len( lst ) * 8 )
 		for r in lst:
 			msg += struct.pack( "d", r )
-		self.__send( msg )
+		sckt = socket.socket( self.kind, socket.SOCK_STREAM )
+		sckt.connect( self.addr )
+		self.__send( sckt, msg )
+		flg, msg = self.__recv( sckt, 1 )
+		sckt.close()
+		while( not flg ):
+			time.sleep( 0.1 )
+			sckt = socket.socket( self.kind, socket.SOCK_STREAM )
+			sckt.connect( self.addr )
+			self.__send( sckt, msg )
+			flg, msg = self.__recv( sckt, 1 )
+			sckt.close()
 
 
-	def recv_r8( self, siz ):
-		return( list( struct.unpack( "%dd"%( siz ), self.__recv( siz * 8 ) ) ) )
+	def recv( self, src, siz ):
+		sckt = socket.socket( self.kind, socket.SOCK_STREAM )
+		sckt.connect( self.addr )
+		self.__send( sckt, b"R%03d%03d00"%( self.node, src ) )
+		flg, msg = self.__recv( sckt, siz * 8 )
+		sckt.close()
+		while( not flg ):
+			time.sleep( 0.1 )
+			sckt = socket.socket( self.kind, socket.SOCK_STREAM )
+			sckt.connect( self.addr )
+			self.__send( sckt, b"R%03d%03d00"%( self.node, src ) )
+			flg, msg = self.__recv( sckt, siz * 8 )
+			sckt.close()
+		return( list( struct.unpack( "%dd"%( siz ), msg ) ) )
 
-
-	def send_i4( self, lst ):
-		msg = b""
-		for r in lst:
-			msg += struct.pack( "i", r )
-		self.__send( msg )
-
-
-	def recv_i4( self, siz ):
-		return( list( struct.unpack( "%di"%( siz ), self.__recv( siz * 4 ) ) ) )
--- tools/helper/__init__.py
+++ tools/helper/__init__.py
@@ -3,26 +3,32 @@ from __future__ import print_function, division
 import	sys
 if( sys.version_info[0] == 2 ):
 	range = xrange
+
 import	bottle
 import	os
+import	re
 import	config
+
+import	qm3.mol
+import	qm3.elements
+import	qm3.utils
+if( "NAMD" in config.MM_engines ):
+	import	qm3.engines.namd
 
 
 try:
 	cwd = os.environ["HELPR_CWD"]
 except:
-	cwd = ""
+	cwd = os.dirname( sys.argv[0] )
 try:
 	uwd = os.environ["HELPR_UWD"]
 except:
-	uwd = ""
+	uwd = os.environ["HOME"]
 try:
 	pdb = os.path.abspath( os.environ["HELPR_COR"] )
-except:
-	pdb = "None"
-try:
 	top = os.path.abspath( os.environ["HELPR_TOP"] )
 except:
+	pdb = "None"
 	top = "None"
 
 
@@ -37,10 +43,23 @@ def __index():
 	pdb_view = "None"
 	if( pdb != "None" ):
 		pdb_view = "<a href=\"/loadpdb\" target=\"_blank\">%s</a>"%( os.path.basename( pdb ) )
+	qm_sel = ""
+	qm_cor = "none"
+	qm_chg = ""
+	qm_env = "block"
+	if( top == "None" and pdb == "None" ):
+		qm_sel = "*"
+		qm_cor = "block"
+		qm_chg = " disabled"
+		qm_env = "none"
 	return( bottle.template( "index.html", 
 		pdb = pdb_view,
 		top = os.path.basename( top ),
 		qm_eng = qm,
+		qm_sel = qm_sel,
+		qm_chg = qm_chg,
+		qm_cor = qm_cor,
+		qm_env = qm_env,
 		mm_eng = mm ) )
 
 
@@ -49,10 +68,7 @@ def __jsmol( sub ):
 	if( sub[-3:] == "png" ):
 		return( bottle.static_file( sub, root = "jsmol", mimetype = "image/png" ) )
 	else:
-		f = open( os.path.join( cwd, "jsmol", sub ), "rt" )
-		r = f.read()
-		f.close()
-		return( r )
+		return( bottle.static_file( sub, root = "jsmol" ) )
 
 
 @bottle.route( "/editor" )
@@ -73,10 +89,209 @@ def __flushpdb():
 		return( bottle.static_file( job, root = fld, mimetype = "application/octet-stream" ) )
 
 
+SP0 = re.compile( "^([0-9]+)$" )
+SP1 = re.compile( "^([0-9]+)-([0-9]+)$" )
+SP2 = re.compile( "^([A-Z0-9]+):([0-9]+)$" )
+SP3 = re.compile( "^([A-Z0-9]+):([0-9]+)-([0-9]+)$" )
+SP4 = re.compile( "^([A-Z0-9]+)/([0-9]+)/(.+)$" )
+SP5 = re.compile( "^([A-Z0-9]+):([0-9]+)@([0-9\.]+)$" )
+def __selection( mol, buf ):
+	sel  = [ False for i in range( mol.natm ) ]
+	_not = False
+	for itm in buf.split():
+		# -- all
+		if( itm == "*" ):
+			for i in range( mol.natm ):
+				sel[i] = True
+		# -- negate ALL selection (at the end...)
+		elif( itm == "not" ):
+			_not = True
+		# -- atom number (NOT C-indexing)
+		elif( SP0.match( itm ) ):
+			sel[int(itm)-1] = True
+		# -- range of atom numbers (NOT C-indexing)
+		elif( SP1.match( itm ) ):
+			rng = [ int( i ) for i in SP1.findall( itm )[0] ]
+			for i in range( rng[0], rng[1] + 1 ):
+				sel[i-1] = True
+		# -- residue number (by chain)
+		elif( SP2.match( itm ) ):
+			rng = SP2.findall( itm )[0]
+			for i in list( mol.indx[rng[0]][int(rng[1])].values() ):
+				sel[i] = True
+		# -- range of residue numbers (by chain)
+		elif( SP3.match( itm ) ):
+			rng = SP3.findall( itm )[0]
+			for i in range( int( rng[1] ), int( rng[2] ) + 1 ):
+				for j in list( mol.indx[rng[0]][i].values() ):
+					sel[j] = True
+		# -- chain / residue_number / atom_label
+		elif( SP4.match( itm ) ):
+			rng = SP4.findall( itm )[0]
+			sel[mol.indx[rng[0]][int(rng[1])][rng[2]]] = True
+		# -- radial selection by residue around chain / residue_number
+		elif( SP5.match( itm ) ):
+			rng = SP5.findall( itm )[0]
+			for i in mol.sph_sel( list( mol.indx[rng[0]][int(rng[1])].values() ), float( rng[2] ) ):
+				sel[i] = True
+		# -- backbone
+		elif( itm == "backbone" ):
+			for i in range( mol.natm ):
+				if( mol.labl[i] in [ "C", "O", "CA", "N" ] ):
+					sel[i] = True
+	if( _not ):
+		for i in range( mol.natm ):
+			sel[i] = not sel[i]
+	return( [ i for i in range( mol.natm ) if sel[i] ] )
+
+
 @bottle.route( "/mkinput", method = "POST" )
 def __mkinput():
-	print( list( bottle.request.forms.allitems() ) )
-	return( "<h1>Input processed!</h1>" )
+	job = os.path.join( uwd, "run_qm3" )
+	try:
+		mol = qm3.mol.molecule( pdb )
+	except:
+		mol = None
+	sel = []
+	# -- selections: QMsel, QMenv, MMmov (possible link_atoms and exclussions...)
+	# -- let's go!
+	f = open( job, "wt" )
+	f.write( """from __future__ import print_function, division
+import	sys
+if( sys.version_info[0] == 2 ):
+	range = xrange
+try:
+	import cPickle as pickle
+except:
+	import pickle
+import os
+import time
+try:
+	import cStringIO
+except:
+	import io as cStringIO
+
+import	qm3.mol
+import	qm3.problem
+""" )
+	# -- imports: QMeng, MMeng, Todo[min_fire pes_scan min_quad nor_mode mep_path dyn_lang]
+	# -- molecule definition (based on pdb or cartesian input provided: QMcor)
+	f.write( """
+
+class my_problem( qm3.problem.template ):
+	def __init__( self ):
+		qm3.problem.template.__init__( self )
+""" )
+	xyz = bottle.request.forms.get( "QMcor" ).strip()
+	if( mol != None and top != "None" ):
+		f.write( """		self.mole = qm3.mol.molecule( "%s" )\n"""%( pdb ) )
+		if( top != "None" ):
+			f.write( """		self.mole.psf_read( "%s" )
+		self.mole.nbnd_read( "@@@@" )
+"""%( top ) )
+	else:
+		f.write( """		f = cStringIO.StringIO( \"\"\"%d\n\n%s\"\"\" )
+		f.seek( 0 )
+		self.mole = qm3.mol.molecule()
+		self.mole.xyz_read( f )
+		f.close()
+		self.mole.guess_atomic_numbers()
+		self.mole.fill_masses()
+		"""%( len( xyz.split() ) // 4, xyz.replace( "\r", "" ) ) )
+	# -- boxl: box_X, box_Y, box_Z
+	tmp = bottle.request.forms.get( "box_X" )
+	try:
+		box = [ float( tmp ), float( tmp ), float( tmp ) ]
+	except:
+		box = []
+	try:
+		tmp = float( bottle.request.forms.get( "box_Y" ) )
+		box[1] = tmp
+	except:
+		pass
+	try:
+		tmp = float( bottle.request.forms.get( "box_Z" ) )
+		box[2] = tmp
+	except:
+		pass
+	if( box != [] ):
+		f.write( """		self.mole.boxl = [ %.2lf, %.2lf, %.2lf ]\n"""%( box[0], box[1], box[2] ) )
+	# -- definition of the active system (full or sele.pk based)
+	if( sel == [] ):
+		f.write( """
+		self.size = 3 * self.mole.natm
+		self.coor = self.mole.coor
+		self.mass = self.mole.mass
+		self.sele = []
+""" )
+	else:
+		f.write( """
+		f = open( "sele.pk", "rb" )
+		self.sele = pickle.load( f )
+		f.close()
+		self.size = 3 * len( self.sele )
+		self.coor = []
+		self.mass = []
+		for i in self.sele:
+			i3 = i * 3
+			self.coor += self.mole.coor[i3:i3+3][:]
+			self.mass.append( self.mole.mass[i] )
+""" )
+	# -- generic
+	f.write( """
+		self.func = 0.0
+		self.grad = []
+		self.hess = []
+""" )
+	# -- engines: QMeng (QMchg QMmul QMmet QMbas), MMeng, Other (like restraints...)
+	# -- zero QM charges on the MM engine (if any...)
+	# -- update_coor method
+	f.write( """
+
+	def update_coor( self ):
+		if( self.sele != [] ):
+			for i in range( len( self.sele ) ):
+				ii = 3 * self.sele[i]
+				jj = 3 * i
+				for j in [ 0, 1, 2 ]:
+					self.mole.coor[ii+j] = self.coor[jj+j]
+""" )
+	# -- get_func method
+	f.write( """
+
+	def get_func( self ):
+		self.update_coor()
+		self.mole.func = 0.0
+""" )
+	# -- get_grad method
+	f.write( """
+
+	def get_grad( self ):
+		self.update_coor()
+		self.mole.func = 0.0
+		self.mole.grad = [ 0.0 for i in range( 3 * self.mole.natm ) ]
+""" )
+	# -- get_hess method
+	f.write( """
+
+	def get_hess( self ):
+		self.update_coor()
+		self.mole.func = 0.0
+		self.mole.grad = [ 0.0 for i in range( 3 * self.mole.natm ) ]
+""" )
+	# -- object generation
+	f.write( """
+
+
+
+
+obj = my_problem()
+
+""" )
+	# -- actions
+	# -- stop engines (if needed...)
+	f.close()
+	return( "<h1>Script generated: %s</h1>"%( job ) )
 
 
 bottle.run( host = "localhost", port = 8080 )
