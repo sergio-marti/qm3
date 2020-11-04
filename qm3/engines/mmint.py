aa0134c5e2b1e424c43652548b36033e4b0abd01
Sergio Martí <smarti@uji.es>
2020-11-04 21:42:51 +0100

    ...

--- qm3/engines/mmint.py
+++ qm3/engines/mmint.py
@@ -10,28 +10,98 @@ import qm3.fio
 from qm3.engines._mmint import *
 
 
-# >> add a parser function for charmm/par, dynamo/opls?
+def p_sander( data ):
+    out = {}
+    if( type( data ) == list ):
+        lst = data[:]
+    else:
+        lst = [ data ]
+    for fname in lst:
+        f = qm3.fio.open_r( fname )
+        l = f.readline()
+        while( l != "" ):
+            if( l[0:4] == "MOD4" or l[0:4] == "NONB" ):
+                t = f.readline().split()
+                while( len( t ) >= 3 ):
+                    out[t[0].upper()] = [ math.sqrt( float( t[2] ) * qm3.constants.K2J ), float( t[1] ) ]
+                    t = f.readline().split()
+            l = f.readline()
+        qm3.fio.close( f, fname )
+    return( out )
 
-def non_bonded( mol, fname ):
+
+def p_charmm( data ):
+    out = {}
+    if( type( data ) == list ):
+        lst = data[:]
+    else:
+        lst = [ data ]
+    for fname in lst:
+        f = qm3.fio.open_r( fname )
+#        l = f.readline()
+#        while( l != "" ):
+#            if( l[0:4] == "MOD4" ):
+#                t = f.readline().split()
+#                while( len( t ) >= 3 ):
+#                    out[t[0].upper()] = [ math.sqrt( float( t[2] ) * qm3.constants.K2J ), float( t[1] ) ]
+#                    t = f.readline().split()
+#            l = f.readline()
+        qm3.fio.close( f, fname )
+    return( out )
+
+
+def p_dynamo( data ):
+    out = {}
+    if( type( data ) == list ):
+        lst = data[:]
+    else:
+        lst = [ data ]
+    for fname in lst:
+        f = qm3.fio.open_r( fname )
+#        l = f.readline()
+#        while( l != "" ):
+#            if( l[0:4] == "MOD4" ):
+#                t = f.readline().split()
+#                while( len( t ) >= 3 ):
+#                    out[t[0].upper()] = [ math.sqrt( float( t[2] ) * qm3.constants.K2J ), float( t[1] ) ]
+#                    t = f.readline().split()
+#            l = f.readline()
+        qm3.fio.close( f, fname )
+    return( out )
+
+
+def p_simple( data ):
+    out = {}
+    pat = re.compile( "([^\ ^\n]+)[\ ]+([0-9\.]+)[\ ]+([0-9\.]+)" )
+    if( type( data ) == list ):
+        lst = data[:]
+    else:
+        lst = [ data ]
+    for fname in lst:
+        f = qm3.fio.open_r( fname )
+        for i,j,k in pat.findall( f.read() ):
+            out[i.upper()] = [ math.sqrt( float( j ) * qm3.constants.K2J ), float( k ) ]
+        qm3.fio.close( f, fname )
+    return( out )
+
+
+def non_bonded( mol, data, parser = p_simple ):
     out = True
     if( mol.type == [] ):
         print( "- Molecule types undefined!" )
         return( False )
     mol.epsi = []
     mol.rmin = []
-    nbd = {}
-    f = qm3.fio.open_r( fname )
-    for i,j,k in re.compile( "([A-Z0-9]+)[\ ]+([0-9\.]+)[\ ]+([0-9\.]+)" ).findall( f.read() ):
-        nbd[i] = [ math.sqrt( float( j ) * qm3.constants.K2J ), float( k ) ]
-    qm3.fio.close( f, fname )
+    nbd = parser( data )
     for i in range( mol.natm ):
-        if( mol.type[i] in nbd ):
-            mol.epsi.append( nbd[mol.type[i]][0] )
-            mol.rmin.append( nbd[mol.type[i]][1] )
+        t = mol.type[i].upper()
+        if( t in nbd ):
+            mol.epsi.append( nbd[t][0] )
+            mol.rmin.append( nbd[t][1] )
         else:
             mol.epsi.append( None )
             mol.rmin.append( None )
-            print( "- Atom index %d misses Non-Bonded data..."%( i+1 ) )
+            print( "- Unknown atom type [%s] at index: %d"%( t, i+1 ) )
             out = False
     return( out )
 
