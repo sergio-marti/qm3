7d78ef28b53a4f17d9704bd90a64c7a01c2a00c1
Sergio Martí <smarti@uji.es>
2020-11-25 10:01:41 +0100

    ...

--- qm3/utils/volume.c
+++ qm3/utils/volume.c
@@ -10,13 +10,11 @@
 #define max(a,b) (((a)>(b))?(a):(b))
 
 
+// ------------------------------------------------------------------------------
+
 typedef struct ptn_node { long i, j, k; struct ptn_node *n; } ptn_lst;
 typedef struct sph_node { long r; ptn_lst *p; struct sph_node *n; } sph_lst;
 typedef struct { long _i0, _if, *npt; char *grd; double dsp, *bmn, *crd; sph_lst **hsh; } targ;
-
-
-//pthread_mutex_t    lock;
-
 
 
 void* __fill_atoms( void *args ) {
@@ -32,9 +30,7 @@ void* __fill_atoms( void *args ) {
     	k  = (long)( ( a->crd[l3+2] - a->bmn[2] ) / a->dsp );
     	c  = a->hsh[l]->p;
     	while( c != NULL ) {
-//pthread_mutex_lock( &lock );
     		a->grd[ (i + c->i) + (j + c->j) * a->npt[0] + (k + c->k) * n01 ] += 1;
-//pthread_mutex_unlock( &lock );
     		c = c->n;
     	}
     }
@@ -47,9 +43,9 @@ static PyObject* __molecular_grid( PyObject *self, PyObject *args ){
     long		i, j, k, l, l3, cpu, siz, rad, rd2, *rng, nit, knd = 0;
     double		mad, tmp, cnt;
     double		cte = 4.1887902047863905;
-    double		vol = 0.0, err = 0.0, exc = 0.0;
+    double		vol = 0.0, err = 0.0;
     double		bmax[3] = { -9999., -9999., -9999. };
-    double		bmin[3] = { +9999., +9999., +9999. };;
+    double		bmin[3] = { +9999., +9999., +9999. };
     long		npt[3];
     char		*grd;
     double		*crd, dsp, d3;
@@ -166,40 +162,37 @@ fprintf(stderr,"ERR: %.3lf _A^3\n",err*siz);
     	}
     	for( l = 0; l < cpu; l++ ) pthread_join( pid[l], NULL );
 
-    if( o_flg == Py_True ) {
-    	FILE* fd = fopen( "volume.cube", "wt" );
-    	fprintf( fd, "QM3:\n-- Molecular Volume --\n" );
-    	tmp = 0.52917721092;
-    	fprintf( fd, "%5ld%12.6lf%12.6lf%12.6lf\n", siz, bmin[0] / tmp, bmin[1] / tmp, bmin[2] / tmp );
-    	fprintf( fd, "%5ld%12.6lf%12.6lf%12.6lf\n", npt[0], dsp / tmp, 0.0, 0.0 );
-    	fprintf( fd, "%5ld%12.6lf%12.6lf%12.6lf\n", npt[1], 0.0, dsp / tmp, 0.0 );
-    	fprintf( fd, "%5ld%12.6lf%12.6lf%12.6lf\n", npt[2], 0.0, 0.0, dsp / tmp );
-    	for( l = 0; l < siz; l++ )
-    		fprintf( fd, "%5ld%12.6lf%12.6lf%12.6lf%12.6lf\n", (long)0, 0.0,
-    			crd[3*l] / tmp, crd[3*l+1] / tmp, crd[3*l+2] / tmp );
-    	nit = npt[0] * npt[1];
-    	l = 0;
-    	for( i = 0; i < npt[0]; i++ ) {
-    		for( j = 0; j < npt[1]; j++ ) {
-    			for( k = 0; k < npt[2]; k++ ) {
-    				if( grd[i+j*npt[0]+k*nit] > 0 ) { vol += 1.0; }
-//    				if( grd[i+j*npt[0]+k*nit] > 1 ) { exc += 1.0; }
-    				fprintf( fd, "%13.5le", (float) grd[i + j * npt[0] + k * nit] );
-    				l++;
-    				if( l%6 == 0 ) { fprintf( fd, "\n" ); }
-    			}
-    		}
-    	}
-    	fclose( fd );
-    } else {
-    	for( l = 0; l < npt[0] * npt[1] * npt[2]; l++ ) {
-    		if( grd[l] > 0 ) { vol += 1.0; }
-//    		if( grd[l] > 1 ) { exc += 1.0; }
-    	}
-    }
-
-//fprintf(stderr,"EXC: %lf _A^3\n",exc*d3);
-fprintf(stderr,"VOL: %lf _A^3\n",vol*d3);
+    	if( o_flg == Py_True ) {
+    		FILE* fd = fopen( "volume.cube", "wt" );
+	    	fprintf( fd, "QM3:\n-- Molecular Volume --\n" );
+	    	tmp = 0.52917721092;
+	    	fprintf( fd, "%5ld%12.6lf%12.6lf%12.6lf\n", siz, bmin[0] / tmp, bmin[1] / tmp, bmin[2] / tmp );
+	    	fprintf( fd, "%5ld%12.6lf%12.6lf%12.6lf\n", npt[0], dsp / tmp, 0.0, 0.0 );
+	    	fprintf( fd, "%5ld%12.6lf%12.6lf%12.6lf\n", npt[1], 0.0, dsp / tmp, 0.0 );
+	    	fprintf( fd, "%5ld%12.6lf%12.6lf%12.6lf\n", npt[2], 0.0, 0.0, dsp / tmp );
+	    	for( l = 0; l < siz; l++ )
+	    		fprintf( fd, "%5ld%12.6lf%12.6lf%12.6lf%12.6lf\n", (long)0, 0.0,
+	    			crd[3*l] / tmp, crd[3*l+1] / tmp, crd[3*l+2] / tmp );
+	    	nit = npt[0] * npt[1];
+	    	l = 0;
+	    	for( i = 0; i < npt[0]; i++ ) {
+	    		for( j = 0; j < npt[1]; j++ ) {
+	    			for( k = 0; k < npt[2]; k++ ) {
+	    				if( grd[i+j*npt[0]+k*nit] > 0 ) { vol += 1.0; }
+	    				fprintf( fd, "%13.5le", (float) grd[i + j * npt[0] + k * nit] );
+	    				l++;
+	    				if( l%6 == 0 ) { fprintf( fd, "\n" ); }
+	    			}
+	    		}
+	    	}
+	    	fclose( fd );
+	    } else {
+	    	for( l = 0; l < npt[0] * npt[1] * npt[2]; l++ ) {
+	    		if( grd[l] > 0 ) { vol += 1.0; }
+	    	}
+	    }
+		vol *= d3;
+fprintf(stderr,"VOL: %lf _A^3\n",vol);
 
     	free( pid ); free( rng ); free( arg );
     	free( crd ); 
@@ -219,11 +212,12 @@ fprintf(stderr,"VOL: %lf _A^3\n",vol*d3);
     	free( grd );
 
 fprintf( stderr, "TIM: %ld _sec\n", time( NULL ) - t0 );
-    	return( Py_BuildValue( "d", vol*d3 ) );
+    	return( Py_BuildValue( "d", vol ) );
     }
     Py_INCREF( Py_None ); return( Py_None );
 }
 
+// ------------------------------------------------------------------------------
 
 /*
 This version is not useful for "real" problems in which intersections take
@@ -238,6 +232,7 @@ Follow (someday) these analytical formulas:
     Mathematical Notes, 2001, 69, 421–428
     Nano Studies, 2015, 11, 111-126
 */
+/*
 static PyObject* __molecular_spheres( PyObject *self, PyObject *args ){
     PyObject	*o_xyz, *o_rad;
     long		i, j, i3, j3, siz;
@@ -290,10 +285,157 @@ fprintf( stderr, "TIM: %ld _sec\n", time( NULL ) - t0 );
     }
     Py_INCREF( Py_None ); return( Py_None );
 }
+*/
+
+// ------------------------------------------------------------------------------
+
+
+long __collide( double x, double y, double z, double prb, double dsp, long siz, double *rad, double *crd ) {
+	long	i, f = 0;
+	double	dx, dy, dz, rr;
+
+	for( i = 0; i < siz && f == 0; i++ ) {
+		dx = x - crd[i*3];
+		dy = y - crd[i*3+1];
+		dz = z - crd[i*3+2];
+		rr = rad[i] + prb - dsp;
+		f  = ( dx * dx + dy * dy + dz * dz ) <= ( rr * rr );
+	}
+	return( f );
+}
+
+static PyObject* __cavity_grid( PyObject *self, PyObject *args ){
+    PyObject	*o_xyz, *o_rad, *o_cen, *o_flg;
+    long		i, j, k, l, siz, wr, w2;
+    double		prb = 1.40, vol = 0.0, ri, rj, rk;
+    double		bmax[3] = { -9999., -9999., -9999. };
+    double		bmin[3] = { +9999., +9999., +9999. };
+	long		ci, cj, ck, wi, wj, wk;
+    long		npt[3], cnt[3];
+	long		cub[24] = { 1, 1, 1, 1, 1,-1, 1,-1, 1, 1,-1,-1, -1, 1, 1, -1, 1,-1, -1,-1, 1, -1,-1,-1 };
+    char		***grd;
+    double		*crd, *rad, dsp;
+    time_t		t0;
+
+    o_flg = Py_False;
+    if( PyArg_ParseTuple( args, "OOOd|O", &o_rad, &o_xyz, &o_cen, &dsp, &o_flg ) ) {
+
+    	t0  = time( NULL );
+    	siz = (long) PyList_Size( o_rad );
+    	rad = (double*) malloc( siz * sizeof( double ) );
+    	crd = (double*) malloc( 3 * siz * sizeof( double ) );
+fprintf(stderr,"SIZ: %ld\n",siz);
+fprintf(stderr,"DSP: %lf\n",dsp);
+
+    	for( l = 0; l < siz; l++ ) {
+    		rad[l] = PyFloat_AsDouble( PyList_GetItem( o_rad, l ) );
+    		for( i = 0; i < 3; i++ ) {
+    			crd[l*3+i] = PyFloat_AsDouble( PyList_GetItem( o_xyz, l*3+i ) );
+    			bmin[i] = min( bmin[i], crd[l*3+i] );
+    			bmax[i] = max( bmax[i], crd[l*3+i] );
+    		}
+    	}
+
+    	for( i = 0; i < 3; i++ ) { 
+    		npt[i] = (long)( ( bmax[i] - bmin[i] ) / dsp );
+    		cnt[i] = (long)( ( PyFloat_AsDouble( PyList_GetItem( o_cen, i ) ) - bmin[i] ) / dsp );
+    	}
+fprintf(stderr,"MIN: %8.3lf%8.3lf%8.3lf\n",bmin[0],bmin[1],bmin[2]);
+fprintf(stderr,"MAX: %8.3lf%8.3lf%8.3lf\n",bmax[0],bmax[1],bmax[2]);
+fprintf(stderr,"NPT: %8ld%8ld%8ld\n",npt[0],npt[1],npt[2]);
+fprintf(stderr,"CEN: %8ld%8ld%8ld\n",cnt[0],cnt[1],cnt[2]);
+
+		// allocate memory
+		grd = malloc( sizeof( char **) * npt[0] );
+		for( i = 0; i < npt[0]; i++ ) {
+			grd[i] = malloc( sizeof( char *) * npt[1] );
+			for( j = 0; j < npt[1]; j++ ) {
+				grd[i][j] = malloc( sizeof( char ) * npt[2] );
+				for( k = 0; k < npt[2]; k++ ) {
+					grd[i][j][k] = 0;
+				}
+			}
+		}
+
+		// init
+		grd[cnt[0]][cnt[1]][cnt[2]] = 1;
+
+		// search
+		wr = (long)( ( prb + dsp ) / dsp );
+		w2 = wr * wr;
+		for( l = 0; l < 24; l += 3 ) {
+			for( wi = cnt[0]; wi > 0 && wi < npt[0]; wi += cub[l] ) {
+				ri = bmin[0] + wi * dsp;
+				for( wj = cnt[1]; wj > 0 && wj < npt[1]; wj += cub[l+1] ) {
+					rj = bmin[1] + wj * dsp;
+					for( wk = cnt[2]; wk > 0 && wk < npt[2]; wk += cub[l+2] ) {
+						if( grd[wi][wj][wk] == 1 ) {
+							rk = bmin[2] + wk * dsp;
+							if( __collide( ri, rj, rk, prb, dsp, siz, rad, crd ) == 0 ) {
+								for( i = - wr - 1; i < wr + 1; i++ ) {
+									ci = wi + i;
+									for( j = - wr - 1; j < wr + 1; j++ ) {
+										cj = wj + j;
+										for( k = - wr - 1; k < wr + 1; k++ ) {
+											ck = wk + k;
+											if( ci >= 0 && ci < npt[0] && cj >= 0 && cj < npt[1] && ck >= 0 && ck < npt[2] ) 
+												if( i * i + j * j + k * k <= w2 ) grd[ci][cj][ck] = 1;
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+    	if( o_flg == Py_True ) {
+	    	FILE* fd = fopen( "volume.pdb", "wt" );
+	    	for( i = 1; i < npt[0] - 1; i++ )
+	    		for( j = 1; j < npt[1] - 1; j++ )
+	    			for( k = 1; k < npt[2] - 1; k++ )
+	    				if( grd[i][j][k] == 1 ) { 
+							vol += 1.0;
+							if( ! ( grd[i-1][j][k] == 1 && 
+									grd[i+1][j][k] == 1 && 
+									grd[i][j-1][k] == 1 && 
+									grd[i][j+1][k] == 1 && 
+									grd[i][j][k-1] == 1 && 
+									grd[i][j][k+1] == 1 ) )
+								fprintf( fd, "ATOM      1  H   SRF     1    %8.3lf%8.3lf%8.3lf  1.00  0.00    SRF\n",
+									bmin[0] + i * dsp, bmin[1] + j * dsp, bmin[2] + k * dsp );
+						}
+	    	fclose( fd );
+	    } else {
+	    	for( i = 0; i < npt[0]; i++ )
+	    		for( j = 0; j < npt[1]; j++ )
+	    			for( k = 0; k < npt[2]; k++ )
+	    				if( grd[i][j][k] == 1 )
+							vol += 1.0;
+	    }
+		vol *= dsp * dsp * dsp;
+fprintf(stderr,"VOL: %lf _A^3\n",vol );
+
+    	free( rad ); free( crd );
+		for( i = 0; i < npt[0]; i++ ) {
+			for( j = 0; j < npt[1]; j++ ) free( grd[i][j] );
+			free( grd[i] );
+		}
+		free( grd );
+
+fprintf( stderr, "TIM: %ld _sec\n", time( NULL ) - t0 );
+    	return( Py_BuildValue( "d", vol ) );
+    }
+    Py_INCREF( Py_None ); return( Py_None );
+}
+
+// ------------------------------------------------------------------------------
 
 
 static struct PyMethodDef methods [] = {
-    { "molecular",      (PyCFunction)__molecular_grid, METH_VARARGS },
+    { "molecular", (PyCFunction)__molecular_grid, METH_VARARGS },
+    { "cavity",    (PyCFunction)__cavity_grid, METH_VARARGS },
     { 0, 0, 0 }
 };
 
